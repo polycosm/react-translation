bac1e9186d932471964ad8e7a644e792e982de67
Jesse Myers <jesse.myers@gmail.com>
2023-07-16 08:12:43 -0700

    Add react structure.
     - Initialize with Create React App w/ TypeScript.
     - Strip project to minimum.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from 'react';
+
+export default function App(): ReactElement {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World.
+      </header>
+    </div>
+  );
+}
